c1ded7a630e6362a6050304fa44c6b674ee54e2b
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2025-09-06 20:58:47 +0530

    fix(lru): re-write LRU cache
    The current LRU implementation has several issues this re-write fixes:
    
    1. The get() function takes a mutable reference to self. This is bad
       becuase structs owning an LRUCache must also take a mutable refernce
       to themselves in their functions in order to do *anything* the cache.
    
       This could be solved by wrapping HashMap and SlotMap in RefCell<T>
       but it is unappealing due to the runtime cost that entails. Instead
       we use UnsafeCell which does not do any runtime checks.
    
       This makes LRUCache unsafe for multi-threaded use. In future, if we
       desire thread-safety, we will have to wrap these fields into a RwLock
       or a Mutex.
    
    2. The current LRU implementation uses clone() to copy the keys to store
       keys in both, the linked list and the hashmap. This is bad for two
       reasons:
    
       * The cache is restricted to keys that implement `Clone`.
    
       * The cache keeps two copies of the same key around, which is
         inefficient.
    
    We overcome this by storing references to the keys in our hashmap
    instead of owned keys. This is however not possible to express in safe
    Rust using regular references. Instead, we store pointers to the keys in
    the HashMap.
    
    Since HashMap can't be read from or written to via raw pointers
    directly, we wrap them in a struct `KeyRef` and implement the traits
    that HashMap operations require.
    
    Users of LruCache should not have to be aware of presence of any of this
    and this is why they should be able to read a LruCache<K, V> via any
    type Q that can be borrowed from K. (K: Borrow<Q>).
    
    Unfortunately, we cannot write a blanket implementation such as:
    
    	impl<K, Q> Borrow<Q> for KeyRef<K>
    	where
    	    K: Borrow<Q>
    
    because that conflicts with a blanket implementation in stdlib.
    
    We workaround this using transparent type and writting the blanket
    implementation for it instead.
    
    	#[repr(transparent)]
    	struct KeyWrapper<K: ?Sized>(K);
    
    	impl<K, Q> Borrow<KeyWrapper<Q>> for KeyRef<K>
    	where
    	    K: Borrow<Q>
    
    And now in our implmentation of get(), we can do:
    
    	pub fn get<Q>(&self, key: &Q) -> Option<&V>
    	where
    	    K: Borrow<Q>,
    	{
    	    let key = KeyWrapper::from_ref(key);
    	    let _ = map.get_mut(key);
    	    ...
    	}
    
    This also has an added benifit of being able to query the LRU cache with
    any type Q as long as K: Borrow<Q>. LruCache<String, V> can be queried
    using &str for example.

--- src/datastructure/lru.rs
+++ src/datastructure/lru.rs
@@ -1,58 +1,256 @@
-use std::{collections::HashMap, hash::Hash};
+//! A simple LRU cache implementation.
+
+use std::cell::UnsafeCell;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::{borrow::Borrow, hash::Hasher};
 
 use crate::datastructure::slotmap::{NodeHandle, SlotMap};
 
+struct KeyRef<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    key: *const K,
+}
+
+impl<K> KeyRef<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    fn from_ref(key: &K) -> Self {
+        Self {
+            key: key as *const K,
+        }
+    }
+}
+
+impl<K> PartialEq for KeyRef<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    fn eq(&self, other: &Self) -> bool {
+        unsafe { *self.key == *other.key }
+    }
+}
+
+impl<K> Eq for KeyRef<K> where K: Eq + Hash + ?Sized {}
+
+impl<K> Hash for KeyRef<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        unsafe { &*self.key }.hash(state);
+    }
+}
+
+/// A wrapper to allow a blanket implmentation of Borrow without conflicting with the stdlib blanket impl
+/// of Borrow.
+#[repr(transparent)]
+#[allow(unused)] // Since this is transparent, we don't contruct it directly but cast from K to Self
+struct KeyWrapper<K: ?Sized>(K);
+
+impl<K> KeyWrapper<K>
+where
+    K: ?Sized,
+{
+    fn from_ref(key: &K) -> &Self {
+        // Safety: KeyWrapper is repr(transparent) so its OK to cast from K to Self
+        unsafe { &*(key as *const K as *const Self) }
+    }
+}
+
+impl<K, Q> Borrow<KeyWrapper<Q>> for KeyRef<K>
+where
+    K: Borrow<Q> + Eq + Hash + ?Sized,
+    Q: ?Sized,
+{
+    fn borrow(&self) -> &KeyWrapper<Q> {
+        KeyWrapper::from_ref(unsafe { &*self.key }.borrow())
+    }
+}
+
+impl<K> PartialEq for KeyWrapper<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl<K> Eq for KeyWrapper<K> where K: Eq + Hash + ?Sized {}
+
+impl<K> Hash for KeyWrapper<K>
+where
+    K: Eq + Hash + ?Sized,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
+/// A simple LRU cache implementation.
+///
+/// # Example
+/// ```ignore
+/// let mut cache = LruCache::new(2);
+///
+/// cache.put("foo", "bar");
+/// cache.put("baz", "qux");
+///
+/// assert_eq!(cache.get(&"foo"), Some(&"bar"));
+/// assert_eq!(cache.get(&"baz"), Some(&"qux"));
+///
+/// cache.put("quux", "corge");
+///
+/// assert_eq!(cache.get(&"foo"), None);
+/// assert_eq!(cache.get(&"baz"), Some(&"qux"));
+/// assert_eq!(cache.get(&"quux"), Some(&"corge"));
+/// ```
 pub struct LruCache<K, V>
 where
-    K: Eq + Hash + Clone,
+    K: Eq + Hash,
 {
-    map: HashMap<K, (V, NodeHandle<K>)>,
-    list: SlotMap<K>,
+    // To be able to mutably borrow these without a mutable reference to self we use
+    // UnsafeCell.
+    //
+    // User should not have to take a mutable reference to self read from the cache using
+    // Self::get. But we do need to mutably borrow these without to move the read values
+    // to the front of the list.
+    map: UnsafeCell<HashMap<KeyRef<K>, (V, NodeHandle<K>)>>,
+    list: UnsafeCell<SlotMap<K>>,
+
     capacity: usize,
 }
 
 impl<K, V> LruCache<K, V>
 where
-    K: Eq + Hash + Clone,
+    K: Eq + Hash,
 {
+    /// Creates a new `LruCache` with the given capacity.
+    /// The cache will evict the least recently used key when the capacity is exceeded.
     pub fn new(capacity: usize) -> Self {
         Self {
-            map: HashMap::with_capacity(capacity),
-            list: SlotMap::new_with_capacity(capacity),
+            map: UnsafeCell::new(HashMap::with_capacity(capacity)),
+            list: UnsafeCell::new(SlotMap::new_with_capacity(capacity)),
             capacity,
         }
     }
+}
 
-    pub fn get(&mut self, key: &K) -> Option<&V> {
-        let entry = self.map.get_mut(key);
+impl<K, V> LruCache<K, V>
+where
+    K: Eq + Hash,
+{
+    /// Returns a refence to the value of the key if it exists in the cache or None
+    /// otherwise.
+    ///
+    /// If the key is found, it is moved to the front of the LRU list.
+    ///
+    /// # Example
+    /// ```ignore
+    /// let mut cache = LruCache::new(2);
+    ///
+    /// cache.put("foo", "bar");
+    ///
+    /// assert_eq!(cache.get(&"foo"), Some(&"bar"));
+    /// assert_eq!(cache.get(&"baz"), None);
+    /// ```
+    pub fn get<Q>(&self, key: &Q) -> Option<&V>
+    where
+        Q: Hash + Eq + ?Sized,
+        K: Borrow<Q>,
+    {
+        // Safety: we are sure this is OK becuase we only have one mutable reference
+        // to the map and list at a time.
+        let map = unsafe { self.map_mut() };
+        let list = unsafe { self.list_mut() };
 
-        if let Some(entry) = entry {
-            let new_node = self
-                .list
-                .move_to_front(entry.1)
-                .expect("BUG(LRU): node existed in the hashmap but not present in the list.");
+        let key = KeyWrapper::from_ref(key);
 
-            entry.1 = new_node;
+        let (value, node) = map.get_mut(key)?;
 
-            Some(&entry.0)
-        } else {
-            None
-        }
+        let new_node = list
+            .move_to_front(*node)
+            .expect("BUG(LRU): node existed in the hashmap but not present in the list.");
+
+        *node = new_node;
+
+        Some(value)
     }
 
+    /// Puts a new key-value pair into the cache.
+    /// The value is inserted at the front of the LRU list.
+    ///
+    /// If the key already exists in the cache, the value is updated and the
+    /// key is moved to the front of the LRU list.
+    ///
+    /// If the cache is at capacity and the key is not already present, the least
+    /// recently used key is removed.
     pub fn put(&mut self, key: K, value: V) {
-        if self.map.len() >= self.capacity {
-            let last = self
-                .list
+        // Safety: we are sure this is OK becuase we only have one mutable reference
+        // to the map and list at a time.
+        let map = unsafe { self.map_mut() };
+        let list = unsafe { self.list_mut() };
+
+        let key_wrapper = KeyWrapper::from_ref(&key);
+
+        if let Some((old_value, node)) = map.get_mut(key_wrapper) {
+            let new_node = list
+                .move_to_front(*node)
+                .expect("BUG(LRU): node existed in the hashmap but not present in the list.");
+            *node = new_node;
+            *old_value = value;
+
+            return;
+        }
+
+        // Make room for the new key by removing the least recently used key
+        // if we are at capacity.
+        if map.len() >= self.capacity {
+            let last = list
                 .pop_back()
                 .expect("BUG(LRU): list is empty while map capacity is exceeded");
-            self.map.remove(&last);
+
+            map.remove(&KeyRef::from_ref(&last));
         }
 
-        let node = self.list.push_front(key.clone());
+        let new_node = list.push_front(key);
 
-        self.map.insert(key, (value, node));
+        let key = list
+            .get(new_node)
+            .expect("BUG(LRU): a node just pushed to the list is not present in the list.");
+
+        map.insert(KeyRef::from_ref(key), (value, new_node));
     }
+}
+
+impl<K, V> LruCache<K, V>
+where
+    K: Eq + Hash,
+{
+    #[allow(clippy::mut_from_ref)]
+    unsafe fn list_mut(&self) -> &mut SlotMap<K> {
+        unsafe { &mut *self.list.get() }
+    }
+
+    #[allow(clippy::mut_from_ref)]
+    unsafe fn map_mut(&self) -> &mut HashMap<KeyRef<K>, (V, NodeHandle<K>)> {
+        unsafe { &mut *self.map.get() }
+    }
+}
+
+// Safety: No one besides us has the `UnsafeCell`. Therefore it is
+// safe to transfer LruCache to other thread as long as K and V can both
+// be as well.
+unsafe impl<K, V> Send for LruCache<K, V>
+where
+    K: Send,
+    V: Send,
+    K: Eq + Hash,
+{
 }
 
 #[cfg(test)]
@@ -74,5 +272,23 @@ mod tests {
         assert_eq!(cache.get(&"foo"), None);
         assert_eq!(cache.get(&"baz"), Some(&"qux"));
         assert_eq!(cache.get(&"quux"), Some(&"corge"));
+    }
+
+    #[test]
+    fn test_updating_existing_key_at_capacity_does_not_remove_any_keys() {
+        let mut cache = LruCache::new(2);
+
+        // evicted
+        cache.put("key1", "value1");
+
+        cache.put("key2", "value2");
+        cache.put("key3", "value3");
+
+        cache.put("key3", "value3 new");
+
+        assert_eq!(cache.get(&"key1"), None);
+
+        assert_eq!(cache.get(&"key2"), Some(&"value2"));
+        assert_eq!(cache.get(&"key3"), Some(&"value3 new"));
     }
 }
